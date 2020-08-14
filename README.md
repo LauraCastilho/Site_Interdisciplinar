040eafc2d76aeba63ce1302307b84e7d7a1f6104
Laura <laura.maravilinda.325@gmail.com>
2020-08-14 01:30:08 -0300

    master

--- README.md
+++
@@ -1,2 +0,0 @@
-# Site
-Trabalho interdisciplinar de DDW, Educação Física e Biologia
--- js/script.js
+++ js/script.js
@@ -7,6 +7,8 @@
     $(window).scroll(function () {
         if ($('.navigation').offset().top > 100) {
             $('.navigation').addClass('nav-bg');
+        } else {
+            $('.navigation').removeClass('nav-bg');
         }
     });
 
